5c53f1ce81591269682bcf5fe2c99193405731b7
David Lundström <david@mediavalet.se>
2023-02-03 21:31:35 +0100

    3rd commit

--- server.js
+++ server.js
@@ -11,16 +11,11 @@ process.on('uncaughtException', (err) => {
 
 const app = require('./app');
 
-const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD
-);
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 mongoose.connect(DB).then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
-const server = app.listen(port, () =>
-  console.log(`App running on port ${port}...`)
-);
+const server = app.listen(port, () => console.log(`App running on port ${port}...`));
 
 process.on('unhandledRejection', (err) => {
   console.log('UNHANDLED REJECTION! Shutting down...');
