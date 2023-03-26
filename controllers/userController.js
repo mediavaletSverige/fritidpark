f61e15d546c6df91e938671ae459f6cd9807a779
David Lundström <david@mediavalet.se>
2023-03-26 13:39:55 +0200

    23th commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,7 @@ const sharp = require('sharp');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
-const factory = require('./handlerFactory');
+const factory = require('./factoryController');
 
 const multerStorage = multer.memoryStorage();
 
